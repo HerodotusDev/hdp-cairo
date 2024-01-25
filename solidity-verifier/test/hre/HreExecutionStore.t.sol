8e83078392ef836599311bfaa4f10395eea5dfe7
Marcello Bardus <marcellobardus8@gmail.com>
2024-01-25 09:22:50 +0000

    wip

--- solidity-verifier/test/hre/HreExecutionStore.t.sol
+++ solidity-verifier/test/hre/HreExecutionStore.t.sol
@@ -32,6 +32,9 @@ contract MockAggregatorsFactory is IAggregatorsFactory {
 }
 
 contract HreExecutionStoreTest is Test {
+    using BlockSampledDatalakeCodecs for BlockSampledDatalake;
+    using ComputationalTaskCodecs for ComputationalTask;
+
     HreExecutionStore private hre;
 
     IFactsRegistry private factsRegistry;
@@ -45,8 +48,8 @@ contract HreExecutionStoreTest is Test {
 
     function test_requestExecutionOfTaskWithBlockSampledDatalake() public {
         BlockSampledDatalake memory datalake = BlockSampledDatalake({
-            blockRangeStart: 1000,
-            blockRangeEnd: 2000,
+            blockRangeStart: 10399990,
+            blockRangeEnd: 10400000,
             increment: 1,
             sampledProperty: BlockSampledDatalakeCodecs
                 .encodeSampledPropertyForHeaderProp(15)
@@ -60,5 +63,55 @@ contract HreExecutionStoreTest is Test {
             datalake,
             computationalTask
         );
+
+        // FFI
+        bytes[] memory encodedDatalakes = new bytes[](4);
+        encodedDatalakes[0] = datalake.encode();
+        encodedDatalakes[1] = datalake.encode();
+        encodedDatalakes[2] = datalake.encode();
+        encodedDatalakes[3] = datalake.encode();
+
+        bytes[] memory encodedComputationalTasks = new bytes[](4);
+        encodedComputationalTasks[0] = ComputationalTask({
+            aggregateFnId: uint256(bytes32("avg")),
+            aggregateFnCtx: ""
+        }).encode();
+        encodedComputationalTasks[1] = ComputationalTask({
+            aggregateFnId: uint256(bytes32("sum")),
+            aggregateFnCtx: ""
+        }).encode();
+        encodedComputationalTasks[2] = ComputationalTask({
+            aggregateFnId: uint256(bytes32("min")),
+            aggregateFnCtx: ""
+        }).encode();
+        encodedComputationalTasks[3] = ComputationalTask({
+            aggregateFnId: uint256(bytes32("max")),
+            aggregateFnCtx: ""
+        }).encode();
+
+        (bytes32 tasksRoot, bytes32 resultsRoot) = processBatchThroughFFI(
+            encodedDatalakes,
+            encodedComputationalTasks
+        );
+    }
+
+    function processBatchThroughFFI(
+        bytes[] memory encodedDatalakes,
+        bytes[] memory encodedComputationalTasks
+    ) internal returns (bytes32 tasksRoot, bytes32 resultsRoot) {
+        // Ensure length match
+        require(
+            encodedDatalakes.length == encodedComputationalTasks.length,
+            "Length mismatch"
+        );
+
+        string[] memory inputsExtended = new string[](3);
+        inputsExtended[0] = "python3 ./helpers/printer.py";
+        inputsExtended[1] = string(abi.encode(encodedDatalakes));
+        inputsExtended[2] = string(abi.encode(encodedComputationalTasks));
+
+        console.logBytes(abi.encode(encodedDatalakes));
+        console.logBytes(abi.encode(encodedComputationalTasks));
+        bytes memory outputExtended = vm.ffi(inputsExtended);
     }
 }
