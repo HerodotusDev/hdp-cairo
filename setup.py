37a19c85d5cf1ee554e2760469bf0212ffde72d9
Pia <gayeongparkk@gmail.com>
2024-05-15 21:51:39 +0900

    fix

--- setup.py
+++ setup.py
@@ -7,16 +7,18 @@ with open('tools/make/requirements.txt') as f:
 setup(
     name="hdp-cairo",
     version="0.0.1",
-    packages=["hdp_bootloader", "tools", "hdp_cairo_programs"],
+    packages=["hdp_bootloader", "tools", "tests", "hdp_cairo_programs" ],
     install_requires=requirements,
     package_dir={
         "tools": "tools",
+        "tests": "tests",
         "hdp_cairo_programs": "build/compiled_cairo_files",
         "hdp_bootloader": "packages/hdp_bootloader",
     },
     zip_safe=False,
     package_data={
         "tools": ["*/*.py"],
+        "tests": ["*/*.py"],
         "hdp_cairo_programs": ["hdp.json", "simple_linear_regression.sierra.json"],
         "hdp_bootloader": ["*.cairo", "*/*.cairo", "*/*.py"],
     },
