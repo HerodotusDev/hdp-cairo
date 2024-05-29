896de69241cb5605f3f476bbd28f9d9ada88a7dc
Bartosz Nowak <bartekn337@gmail.com>
2024-05-29 18:21:56 +0200

    fix

--- tools/make/setup.sh
+++ tools/make/setup.sh
@@ -52,6 +52,7 @@ fi
 # Create virtual environment
 echo "Creating virtual environment..."
 python"$PYTHON_VERSION" -m venv "$VENV_PATH"
+echo 'export PYTHONPATH="$PWD:$PYTHONPATH"' >> "$VENV_PATH/bin/activate"
 source "$VENV_PATH/bin/activate" || { echo "Failed to activate virtual environment."; exit 1; }
 
 # Update dependencies
