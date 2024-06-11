c1150e4924f8bf44b1829be3e5b22d8c2a952c6f
Bartosz Nowak <bartekn337@gmail.com>
2024-06-11 18:02:58 +0200

    module obj refactor

--- packages/contract_bootloader/objects.py
+++ packages/contract_bootloader/objects.py
@@ -2,10 +2,8 @@ from typing import List
 import marshmallow_dataclass
 from dataclasses import field
 import marshmallow.fields as mfields
+from contract_bootloader.contract_class.contract_class import CompiledClass
 from starkware.starkware_utils.validated_dataclass import ValidatedMarshmallowDataclass
-from starkware.starkware_utils.validated_dataclass import (
-    ValidatedMarshmallowDataclass,
-)
 from starkware.starkware_utils.marshmallow_dataclass_fields import (
     IntAsHex,
     additional_metadata,
@@ -14,11 +12,9 @@ from starkware.starkware_utils.marshmallow_dataclass_fields import (
 
 @marshmallow_dataclass.dataclass(frozen=True)
 class Module(ValidatedMarshmallowDataclass):
-    class_hash: int = field(
-        metadata=additional_metadata(marshmallow_field=IntAsHex(required=True))
-    )
     inputs: List[int] = field(
         metadata=additional_metadata(
             marshmallow_field=mfields.List(IntAsHex(), required=True)
         )
     )
+    module_class: CompiledClass
