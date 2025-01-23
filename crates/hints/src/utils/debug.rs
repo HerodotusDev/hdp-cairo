df7161a7b8492c08c0f5dee4f79090d40aaa24fd
petscheit <etscheitp@gmail.com>
2025-01-23 11:40:49 +0100

    chore: fmt + clippy

--- crates/hints/src/utils/debug.rs
+++ crates/hints/src/utils/debug.rs
@@ -1,8 +1,5 @@
 use cairo_vm::{
-    hint_processor::builtin_hint_processor::{
-        builtin_hint_processor_definition::HintProcessorData,
-        hint_utils::{get_integer_from_var_name},
-    },
+    hint_processor::builtin_hint_processor::{builtin_hint_processor_definition::HintProcessorData, hint_utils::get_integer_from_var_name},
     types::exec_scope::ExecutionScopes,
     vm::{errors::hint_errors::HintError, vm_core::VirtualMachine},
     Felt252,
@@ -43,7 +40,6 @@ pub fn print_string(
     hint_data: &HintProcessorData,
     _constants: &HashMap<String, Felt252>,
 ) -> Result<(), HintError> {
-
     let value = get_integer_from_var_name("value", vm, &hint_data.ids_data, &hint_data.ap_tracking)?;
     let bytes = value.to_bytes_be();
     let ascii = String::from_utf8_lossy(&bytes);
