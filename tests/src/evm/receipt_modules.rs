f576ecd716c9dfaae8a7b5d519245ae8e966efe4
Bartosz Nowak <bartekn337@gmail.com>
2025-02-12 11:33:12 +0100

    cairo1 module rename

--- tests/src/evm/receipt_modules.rs
+++ tests/src/evm/receipt_modules.rs
@@ -1,18 +1,18 @@
 use crate::test_utils::run;
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
-async fn test_modules_receipt_get_status() {
+async fn test_tests_receipt_get_status() {
     run(serde_json::from_slice(include_bytes!(
-        "../../../target/dev/modules_receipts_get_status.compiled_contract_class.json"
+        "../../../target/dev/tests_receipts_get_status.compiled_contract_class.json"
     ))
     .unwrap())
     .await
 }
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
-async fn test_modules_receipt_get_cumulative_gas_used() {
+async fn test_tests_receipt_get_cumulative_gas_used() {
     run(serde_json::from_slice(include_bytes!(
-        "../../../target/dev/modules_receipts_get_and_tx_get.compiled_contract_class.json"
+        "../../../target/dev/tests_receipts_get_and_tx_get.compiled_contract_class.json"
     ))
     .unwrap())
     .await
