e4a8d8bbf1d7e1f75758f0b873e354978f116fe5
petscheit <etscheitp@gmail.com>
2025-02-26 14:03:20 +0100

    chore: rm prints

--- crates/dry_run/src/main.rs
+++ crates/dry_run/src/main.rs
@@ -14,21 +14,17 @@ use types::{error::Error, param::Param, CasmContractClass, HDPDryRunInput};
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 1)]
 async fn main() -> Result<(), Error> {
-    println!("Starting dry run...");
     dotenvy::dotenv().ok();
     tracing_subscriber::fmt::init();
 
     let args = Args::try_parse_from(std::env::args()).map_err(Error::Cli)?;
 
-    println!("Reading compiled module: {:?}", args.compiled_module);
     let compiled_class: CasmContractClass = serde_json::from_slice(&std::fs::read(args.compiled_module).map_err(Error::IO)?)?;
-    println!("Compiled module read successfully");
     let params: Vec<Param> = if let Some(input_path) = args.inputs {
         serde_json::from_slice(&std::fs::read(input_path).map_err(Error::IO)?)?
     } else {
         Vec::new()
     };
-    println!("Params: {:?}", params);
 
     let (syscall_handler, output) = dry_run::run(args.program, HDPDryRunInput { compiled_class, params })?;
 
