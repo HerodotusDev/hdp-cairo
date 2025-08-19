2e0c9840272b3b76a047bef925a4296af35b3cb5
Bartosz Nowak <bartekn337@gmail.com>
2025-08-19 15:45:57 -0300

    refactor(types): Standardize injected state keys on trie_label

--- crates/types/src/keys/injected_state/mod.rs
+++ crates/types/src/keys/injected_state/mod.rs
@@ -1,6 +1,5 @@
-pub mod id_to_root;
+pub mod label;
 pub mod read;
-pub mod root_to_id;
 pub mod write;
 
 use thiserror::Error;
--- crates/types/src/keys/injected_state/read.rs
+++ crates/types/src/keys/injected_state/read.rs
@@ -9,25 +9,25 @@ use crate::cairo::traits::CairoType;
 
 #[derive(Debug, Clone)]
 pub struct CairoKey {
-    pub trie_root: Felt252,
+    pub trie_label: Felt252,
     pub key: Felt252,
 }
 
 impl CairoKey {
     pub fn hash(&self) -> Felt252 {
-        poseidon_hash_many(&[self.trie_root, self.key])
+        poseidon_hash_many(&[self.trie_label, self.key])
     }
 }
 
 impl CairoType for CairoKey {
     fn from_memory(vm: &VirtualMachine, address: Relocatable) -> Result<Self, MemoryError> {
         Ok(Self {
-            trie_root: *vm.get_integer((address + 0)?)?,
+            trie_label: *vm.get_integer((address + 0)?)?,
             key: *vm.get_integer((address + 1)?)?,
         })
     }
     fn to_memory(&self, vm: &mut VirtualMachine, address: Relocatable) -> Result<Relocatable, MemoryError> {
-        vm.insert_value((address + 0)?, self.trie_root)?;
+        vm.insert_value((address + 0)?, self.trie_label)?;
         vm.insert_value((address + 1)?, self.key)?;
         Ok((address + 2)?)
     }
