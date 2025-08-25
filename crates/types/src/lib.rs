5f917e4ca484b13e6c43152340bf245f68635495
lakewik <wikjezioro@op.pl>
2025-08-25 17:02:51 +0200

    Add support for Worldchain and Optimism

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -21,6 +21,10 @@ pub const RPC_URL_STARKNET: &str = "RPC_URL_STARKNET";
 
 pub const ETHEREUM_MAINNET_CHAIN_ID: u128 = 0x1;
 pub const ETHEREUM_TESTNET_CHAIN_ID: u128 = 0xaa36a7;
+pub const OPTIMISM_MAINNET_CHAIN_ID: u128 = 0xa;
+pub const OPTIMISM_TESTNET_CHAIN_ID: u128 = 0xaa37dc;
+pub const WORLDCHAIN_MAINNET_CHAIN_ID: u128 = 0x1e0;
+pub const WORLDCHAIN_TESTNET_CHAIN_ID: u128 = 0x12c1;
 pub const STARKNET_MAINNET_CHAIN_ID: u128 = 0x534e5f4d41494e;
 pub const STARKNET_TESTNET_CHAIN_ID: u128 = 0x534e5f5345504f4c4941;
 
@@ -41,6 +45,10 @@ pub struct HDPInput {
 pub enum ChainProofs {
     EthereumMainnet(evm::Proofs),
     EthereumSepolia(evm::Proofs),
+    OptimismMainnet(evm::Proofs),
+    OptimismSepolia(evm::Proofs),
+    WorldchainMainnet(evm::Proofs),
+    WorldchainSepolia(evm::Proofs),
     StarknetMainnet(starknet::Proofs),
     StarknetSepolia(starknet::Proofs),
 }
@@ -50,6 +58,10 @@ impl ChainProofs {
         match self {
             ChainProofs::EthereumMainnet(_) => 0x1,
             ChainProofs::EthereumSepolia(_) => 0xaa36a7,
+            ChainProofs::OptimismMainnet(_) => 0xa,
+            ChainProofs::OptimismSepolia(_) => 0xaa37dc,
+            ChainProofs::WorldchainMainnet(_) => 0x1e0,
+            ChainProofs::WorldchainSepolia(_) => 0x12c1,
             ChainProofs::StarknetMainnet(_) => 0x534e5f4d41494e,
             ChainProofs::StarknetSepolia(_) => 0x534e5f5345504f4c4941,
         }
