828a927537e945428c265e0e96767c83872fbf38
Bartosz Nowak <bartekn337@gmail.com>
2025-09-03 15:27:03 -0300

    feat(examples): Add individual run scripts
    Adds dedicated  scripts for each example and removes the generic top-level script. This makes running and testing individual examples easier.

--- run.sh
+++
@@ -1,3 +0,0 @@
-cargo run --release --bin dry_run -- -m target/dev/example_blacklist_module.compiled_contract_class.json --print_output --inputs examples/blacklist/input.json
-cargo run --bin fetcher
-cargo run --release --bin sound_run -- -m target/dev/example_blacklist_module.compiled_contract_class.json --print_output --inputs examples/blacklist/input.json --cairo_pie_output pie.zip
