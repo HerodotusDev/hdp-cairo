b6640c40cc5b7cd816928cbd56b4c99272020a0c
Bartosz Nowak <bartekn337@gmail.com>
2025-09-11 12:25:01 -0300

    refactor(tests): Update injected_state tests to load specific state files

--- tests/src/injected_state/modules.rs
+++ tests/src/injected_state/modules.rs
@@ -9,7 +9,7 @@ async fn test_injected_state_boundary_value_testing() {
             "../../../target/dev/tests_boundary_value_testing.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/boundary_value_testing_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -22,7 +22,7 @@ async fn test_injected_state_cross_trie_collision_test() {
             "../../../target/dev/tests_cross_trie_collision_test.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/cross_trie_collision_test_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -36,7 +36,7 @@ async fn test_injected_state_edge_inputs_max() {
             "../../../target/dev/tests_edge_inputs_max.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/edge_inputs_max_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -49,7 +49,7 @@ async fn test_injected_state_empty_trie_operations() {
             "../../../target/dev/tests_empty_trie_operations.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/empty_trie_operations_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -62,7 +62,7 @@ async fn test_injected_state_isolation_root_stability() {
             "../../../target/dev/tests_isolation_root_stability.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/isolation_root_stability_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -75,7 +75,7 @@ async fn test_injected_state_key_value_permutation_testing() {
             "../../../target/dev/tests_key_value_permutation_testing.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/key_value_permutation_testing_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -88,7 +88,7 @@ async fn test_injected_state_multi_trie_deterministic_reads() {
             "../../../target/dev/tests_multi_trie_deterministic_reads.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/multi_trie_deterministic_reads_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -101,7 +101,7 @@ async fn test_injected_state_multiple_key_overrides() {
             "../../../target/dev/tests_multiple_key_overrides.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/multiple_key_overrides_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -114,7 +114,7 @@ async fn test_injected_state_multiple_tries_proofs_test() {
             "../../../target/dev/tests_multiple_tries_proofs_test.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/multiple_tries_proofs_test_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -127,7 +127,7 @@ async fn test_injected_state_non_sequential_proof_verification() {
             "../../../target/dev/tests_non_sequential_proof_verification.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/non_sequential_proof_verification_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -140,7 +140,7 @@ async fn test_injected_state_noop_write_same_value() {
             "../../../target/dev/tests_noop_write_same_value.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/noop_write_same_value_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -153,7 +153,7 @@ async fn test_injected_state_order_independence_two_keys() {
             "../../../target/dev/tests_order_independence_two_keys.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/order_independence_two_keys_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -166,7 +166,7 @@ async fn test_injected_state_override_existing_key() {
             "../../../target/dev/tests_override_existing_key.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/override_existing_key_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -179,7 +179,7 @@ async fn test_injected_state_random_reads_across_multiple_tries() {
             "../../../target/dev/tests_random_reads_across_multiple_tries.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/random_reads_across_multiple_tries_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -192,7 +192,7 @@ async fn test_injected_state_read_duplicate_keys() {
             "../../../target/dev/tests_read_duplicate_keys.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/read_duplicate_keys_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -205,7 +205,7 @@ async fn test_injected_state_read_edge_cases() {
             "../../../target/dev/tests_read_edge_cases.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/read_edge_cases_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -218,7 +218,7 @@ async fn test_injected_state_read_empty_key_list() {
             "../../../target/dev/tests_read_empty_key_list.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/read_empty_key_list_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -232,7 +232,7 @@ async fn test_injected_state_read_from_trie_test() {
             "../../../target/dev/tests_read_from_trie_test.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/read_from_trie_test_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -245,7 +245,7 @@ async fn test_injected_state_read_large_key_set() {
             "../../../target/dev/tests_read_large_key_set.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/read_large_key_set_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -258,7 +258,7 @@ async fn test_injected_state_read_mixed_existing_non_existing() {
             "../../../target/dev/tests_read_mixed_existing_non_existing.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/read_mixed_existing_non_existing_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -271,7 +271,7 @@ async fn test_injected_state_read_non_existent_key() {
             "../../../target/dev/tests_read_non_existent_key.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/read_non_existent_key_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -284,7 +284,7 @@ async fn test_injected_state_read_sequential_key_patterns() {
             "../../../target/dev/tests_read_sequential_key_patterns.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/read_sequential_key_patterns_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -297,7 +297,7 @@ async fn test_injected_state_read_sparse_key_distribution() {
             "../../../target/dev/tests_read_sparse_key_distribution.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/read_sparse_key_distribution_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -310,7 +310,7 @@ async fn test_injected_state_root_found_test() {
             "../../../target/dev/tests_root_found_test.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/root_found_test_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -323,7 +323,7 @@ async fn test_injected_state_root_found_wrong_label_test() {
             "../../../target/dev/tests_root_found_wrong_label_test.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/root_found_wrong_label_test_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -337,7 +337,7 @@ async fn test_injected_state_root_is_zero_test() {
             "../../../target/dev/tests_root_is_zero_test.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/root_is_zero_test_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -350,7 +350,7 @@ async fn test_injected_state_root_not_found_test() {
             "../../../target/dev/tests_root_not_found_test.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/root_not_found_test_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -363,7 +363,7 @@ async fn test_injected_state_single_key_trie_operations() {
             "../../../target/dev/tests_single_key_trie_operations.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/single_key_trie_operations_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -377,7 +377,7 @@ async fn test_injected_state_single_trie_proofs_test() {
             "../../../target/dev/tests_single_trie_proofs_test.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/single_trie_proofs_test_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -390,7 +390,7 @@ async fn test_injected_state_trie_label_collision_handling() {
             "../../../target/dev/tests_trie_label_collision_handling.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/trie_label_collision_handling_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -403,7 +403,7 @@ async fn test_injected_state_trie_state_transition_consistency() {
             "../../../target/dev/tests_trie_state_transition_consistency.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/trie_state_transition_consistency_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -416,7 +416,7 @@ async fn test_injected_state_verify_proofs_multiple_tries_test() {
             "../../../target/dev/tests_verify_proofs_multiple_tries_test.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/verify_proofs_multiple_tries_test_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -429,7 +429,7 @@ async fn test_injected_state_write_alternating_patterns() {
             "../../../target/dev/tests_write_alternating_patterns.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/write_alternating_patterns_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -442,7 +442,7 @@ async fn test_injected_state_write_circular_value_pattern() {
             "../../../target/dev/tests_write_circular_value_pattern.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/write_circular_value_pattern_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -455,7 +455,7 @@ async fn test_injected_state_write_concurrent_key_access() {
             "../../../target/dev/tests_write_concurrent_key_access.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/write_concurrent_key_access_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -468,7 +468,7 @@ async fn test_injected_state_write_edge_cases() {
             "../../../target/dev/tests_write_edge_cases.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/write_edge_cases_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -481,7 +481,7 @@ async fn test_injected_state_write_large_number_of_keys() {
             "../../../target/dev/tests_write_large_number_of_keys.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/write_large_number_of_keys_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -494,7 +494,7 @@ async fn test_injected_state_write_same_value_multiple_times() {
             "../../../target/dev/tests_write_same_value_multiple_times.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/write_same_value_multiple_times_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -507,7 +507,7 @@ async fn test_injected_state_write_sequential_overwrites() {
             "../../../target/dev/tests_write_sequential_overwrites.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/write_sequential_overwrites_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -520,7 +520,7 @@ async fn test_injected_state_write_to_existing_trie() {
             "../../../target/dev/tests_write_to_existing_trie.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/write_to_existing_trie_injected_state.json")).unwrap(),
     )
     .await
 }
@@ -533,7 +533,7 @@ async fn test_injected_state_write_to_new_trie() {
             "../../../target/dev/tests_write_to_new_trie.compiled_contract_class.json"
         ))
         .unwrap(),
-        Some("injected_state.json"),
+        serde_json::from_slice(include_bytes!("modules/write_to_new_trie_injected_state.json")).unwrap(),
     )
     .await
 }
