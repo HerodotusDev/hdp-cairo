4e7eecfc9772d03e9f68cb3fb12a177ecbf55690
Bartosz Nowak <bartekn337@gmail.com>
2025-11-04 09:23:57 +0100

    fix(test): Refactor test runner to parse syscalls and fetch live proofs

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -33,7 +33,7 @@ mod test_utils {
         vm::runners::cairo_runner::{CairoRunner, RunnerMode},
     };
     use dry_hint_processor::syscall_handler::{evm, injected_state, starknet, unconstrained};
-    use fetcher::{proof_keys::ProofKeys, Fetcher};
+    use fetcher::{parse_syscall_handler, Fetcher};
     use hints::vars;
     use syscall_handler::{SyscallHandler, SyscallHandlerWrapper};
     use tracing::debug;
@@ -114,48 +114,17 @@ mod test_utils {
             .unwrap()
             .clone();
 
-        let mut proof_keys = ProofKeys::default();
-        for key in syscall_handler.call_contract_handler.evm_call_contract_handler.key_set {
-            match key {
-                evm::DryRunKey::Account(value) => {
-                    proof_keys.evm.account_keys.insert(value);
-                }
-                evm::DryRunKey::Header(value) => {
-                    proof_keys.evm.header_keys.insert(value);
-                }
-                evm::DryRunKey::Storage(value) => {
-                    proof_keys.evm.storage_keys.insert(value);
-                }
-                evm::DryRunKey::Receipt(value) => {
-                    proof_keys.evm.receipt_keys.insert(value);
-                }
-                evm::DryRunKey::Tx(value) => {
-                    proof_keys.evm.transaction_keys.insert(value);
-                }
-            }
-        }
-
-        // Add Starknet keys
-        for key in syscall_handler.call_contract_handler.starknet_call_contract_handler.key_set {
-            match key {
-                starknet::DryRunKey::Header(value) => proof_keys.starknet.header_keys.insert(value),
-                starknet::DryRunKey::Storage(value) => proof_keys.starknet.storage_keys.insert(value),
-            };
-        }
-
-        // Add injected state keys
-        for (root_hash, actions) in syscall_handler.call_contract_handler.injected_state_call_contract_handler.key_set {
-            proof_keys.injected_state.insert(root_hash, actions);
-        }
+        let proof_keys = parse_syscall_handler(syscall_handler).unwrap();
 
         let fetcher = Fetcher::new(&proof_keys);
         let (
-            evm_proofs_mainnet,
-            evm_proofs_sepolia,
+            eth_proofs_mainnet,
+            eth_proofs_sepolia,
             starknet_proofs_mainnet,
             starknet_proofs_sepolia,
             optimism_proofs_mainnet,
             optimism_proofs_sepolia,
+            unconstrained,
             state_proofs,
         ) = tokio::try_join!(
             fetcher.collect_evm_proofs(ETHEREUM_MAINNET_CHAIN_ID),
@@ -164,14 +133,15 @@ mod test_utils {
             fetcher.collect_starknet_proofs(STARKNET_TESTNET_CHAIN_ID),
             fetcher.collect_evm_proofs(OPTIMISM_MAINNET_CHAIN_ID),
             fetcher.collect_evm_proofs(OPTIMISM_TESTNET_CHAIN_ID),
+            fetcher.collect_unconstrained_data(),
             fetcher.collect_state_proofs(),
         )
         .unwrap();
 
         let program_inputs = HDPInput {
             chain_proofs: vec![
-                ChainProofs::EthereumMainnet(evm_proofs_mainnet),
-                ChainProofs::EthereumSepolia(evm_proofs_sepolia),
+                ChainProofs::EthereumMainnet(eth_proofs_mainnet),
+                ChainProofs::EthereumSepolia(eth_proofs_sepolia),
                 ChainProofs::StarknetMainnet(starknet_proofs_mainnet),
                 ChainProofs::StarknetSepolia(starknet_proofs_sepolia),
                 ChainProofs::OptimismMainnet(optimism_proofs_mainnet),
@@ -180,8 +150,8 @@ mod test_utils {
             params: vec![],
             compiled_class,
             state_proofs,
-            injected_state: injected_state.clone(),
-            unconstrained: Default::default(),
+            injected_state,
+            unconstrained,
         };
 
         // Load the Program
