413c5964ff4befb45f79cb52be9eb6fead0206ec
lakewik <wikjezioro@op.pl>
2025-11-20 23:55:00 +0100

    Add the correct MMR deployment config for decommitment of eth call example on Starknet

--- examples/eth_call/run.sh
+++ examples/eth_call/run.sh
@@ -1,4 +1,4 @@
 scarb build -p example_eth_call &&\
 hdp dry-run -m target/dev/example_eth_call_module.compiled_contract_class.json --print_output &&\
-hdp fetch-proofs &&\
+hdp fetch-proofs --mmr-deployment-config examples/eth_call/mmr_deployment_config.json &&\
 hdp sound-run -m target/dev/example_eth_call_module.compiled_contract_class.json --print_output --cairo_pie ./pie.zip
